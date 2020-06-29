9519f65d683c942c3cf8cff0691325348a1506eb
leksmi <alexdotspb@gmail.com>
2020-06-29 18:55:36 +0300

    Add 9_lists_strings_3.py

---
+++ 9_lists_strings_3.py
@@ -0,0 +1,18 @@
+#
+children = ['arbuzov_2000', 'ivanov_2011', 'petrov_2009', 'Bubnov_2015']
+print(children)
+
+# Регистр важен при сортировке!:
+print(sorted(children))
+
+# Фильтрующая фугкция:
+def sort_by_year(name):
+    year = name.split('_')[-1]
+    return year
+
+
+print(sort_by_year(children[0]))
+
+sorted_children = sorted(children, key=sort_by_year)
+print(sorted_children)
+
