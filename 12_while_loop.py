f799cabc5bab186804d0fc008c8c17e014c9d89b
leksmi <alexdotspb@gmail.com>
2020-07-06 21:03:48 +0300

    Add 12_while_loop.py

---
+++ 12_while_loop.py
@@ -0,0 +1,12 @@
+#
+counter = 0
+while True:
+    print(counter)
+    if counter == 112:
+        break
+    counter += 1
+
+counter = 0
+while counter <= 50:
+    print('Current counter = ', counter)
+    counter += 1
